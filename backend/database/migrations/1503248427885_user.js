87f0a28f58e0dabecd194f99b697b8c929f174a1
Breno Zielinski Galendi <brenogalendi@gmail.com>
2020-06-11 20:26:25 -0300

    Feat: Sign up system

--- backend/database/migrations/1503248427885_user.js
+++ backend/database/migrations/1503248427885_user.js
@@ -9,6 +9,7 @@ class UserSchema extends Schema {
       table.increments();
       table.string("email", 254).notNullable().unique();
       table.string("password", 60).notNullable();
+      table.string("access", 10).notNullable();
       table.timestamps();
     });
   }
